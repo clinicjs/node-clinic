43b8e30ed8052cbefe4d7773dbee22a30fc3d0f0
Andreas Madsen <amwebdk@gmail.com>
2017-10-05 17:18:48 +0200

    implement process state fetching

---
+++ doctor/collect/process-state.js
@@ -0,0 +1,48 @@
+'use strict'
+
+function hrtime2ms (time) {
+  return time[0] * 1e3 + time[1] * 1e-6
+}
+
+class ProcessState {
+  constructor (sampleInterval) {
+    if (typeof sampleInterval !== 'number') {
+      throw TypeError('sample interval must be a number')
+    }
+
+    this.sampleInterval = sampleInterval
+    this.refresh()
+  }
+
+  _sampleDelay (elapsedTime) {
+    return elapsedTime - this.sampleInterval
+  }
+
+  _sampleCpuUsage (elapsedTime) {
+    const elapsedCpuUsage = process.cpuUsage(this._lastSampleCpuUsage)
+    // convert to from µs to ms
+    const elapsedCpuUsageTotal = (
+      elapsedCpuUsage.user + elapsedCpuUsage.system
+    ) / 1000
+
+    return elapsedCpuUsageTotal / elapsedTime
+  }
+
+  refresh () {
+    this._lastSampleTime = process.hrtime()
+    this._lastSampleCpuUsage = process.cpuUsage()
+  }
+
+  sample () {
+    const elapsedTime = hrtime2ms(process.hrtime(this._lastSampleTime))
+
+    return {
+      delay: this._sampleDelay(elapsedTime),
+      cpu: this._sampleCpuUsage(elapsedTime),
+      memory: process.memoryUsage(),
+      handles: process._getActiveHandles().length
+    }
+  }
+}
+
+module.exports = ProcessState

244fda3e249a33a6dbc2bc80967012701f6772c6
Andreas Madsen <amwebdk@gmail.com>
2017-10-07 14:12:11 +0200

    test partial decoding

--- doctor/test/format.test.js
+++ doctor/test/format.test.js
@@ -29,3 +29,43 @@ test('basic encoder-decoder works', function (t) {
 
   encoder.end()
 })
+
+test('partial decoding', function (t) {
+  const state = new ProcessState(1)
+
+  const encoder = new ProcessStateEncoder()
+  const decoder = new ProcessStateDecoder()
+
+  // encode a sample
+  const sample = state.sample()
+  encoder.write(sample)
+  const sampleEncoded = encoder.read()
+
+  // No data, chunk is too small
+  decoder.write(sampleEncoded.slice(0, 20))
+  t.strictEqual(t.read(), null)
+
+  // Ended previuse sample, but a partial remains
+  decoder.write(Buffer.concat([
+    sampleEncoded.slice(20),
+    sampleEncoded.slice(0, 30)
+  ]))
+  t.strictDeepEqual(decoder.read(), sample)
+  t.strictDeepEqual(decoder.read(), null)
+
+  // Ended previuse, no partial remains
+  decoder.write(Buffer.concat([
+    sampleEncoded.slice(30),
+    sampleEncoded
+  ]))
+  t.strictDeepEqual(decoder.read(), sample)
+  t.strictDeepEqual(decoder.read(), sample)
+
+  // No previuse ended
+  decoder.write(sampleEncoded)
+  t.strictDeepEqual(decoder.read(), sample)
+
+  // No more data
+  t.strictEqual(decoder.read(), null)
+  t.end()
+})

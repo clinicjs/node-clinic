6aecda0f70d757919fa8ba2f03ff679ee6cecd1f
Mathias Buus <mathiasbuus@gmail.com>
2018-07-19 08:49:13 -0300

    use @nearform/clinic-flame (#57)
    * use @nearform/clinic-flame
    
    * use clinic-flame/version

--- test/cli-flame-version.test.js
+++ test/cli-flame-version.test.js
@@ -8,7 +8,7 @@ test('clinic flame --version', function (t) {
     t.ifError(err)
     t.strictEqual(
       stdout,
-      `v${require('0x/package.json').version}\n`
+      `v${require('@nearform/clinic-flame/version')}\n`
     )
     t.end()
   })
@@ -19,7 +19,7 @@ test('clinic flame -v', function (t) {
     t.ifError(err)
     t.strictEqual(
       stdout,
-      `v${require('0x/package.json').version}\n`
+      `v${require('@nearform/clinic-flame/version')}\n`
     )
     t.end()
   })

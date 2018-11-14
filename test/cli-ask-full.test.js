fa81a3c6a5bcc5330f789c02f98372621534af56
mrister <mihovil.rister@gmail.com>
2018-11-14 16:43:16 +0000

    Update ask tests oer review

--- test/cli-ask-full.test.js
+++ test/cli-ask-full.test.js
@@ -4,6 +4,7 @@ const test = require('tap').test
 const path = require('path')
 const cli = require('./cli.js')
 const FakeUploadServer = require('./fake-upload-server.js')
+let server
 
 const doctorADirectory = path.resolve(
   __dirname,
@@ -12,35 +13,39 @@ const doctorADirectory = path.resolve(
   '10000.clinic-doctor'
 )
 
-test('clinic ask 10000.clinic-doctor with custom upload url', function (t) {
-  const server = new FakeUploadServer()
+test('Before all', function (t) {
+  server = new FakeUploadServer()
   server.listen(function () {
-    cli({}, [
-      'clinic', 'ask',
-      '--upload-url', server.uploadUrl,
-      '--auth-method', 'simpleSuccess',
-      doctorADirectory
-    ], function (err, stdout) {
-      t.ifError(err)
+    t.plan(1)
+    t.ok(server)
+  })
+})
 
-      t.strictDeepEqual(stdout.trim().split('\n'), [
-        `Uploading private data for user test@test.com for ${doctorADirectory} and ${doctorADirectory}.html to ${server.uploadUrl}`,
-        `The data has been uploaded to private area for user test@test.com`,
-        `Thanks for contacting NearForm, we will reply as soon as possible.`
-      ])
+test('clinic ask 10000.clinic-doctor with custom upload url', function (t) {
+  cli({}, [
+    'clinic', 'ask',
+    '--upload-url', server.uploadUrl,
+    '--auth-method', 'simpleSuccess',
+    doctorADirectory
+  ], function (err, stdout) {
+    t.plan(3)
+    t.ifError(err)
 
-      t.strictDeepEqual(server.requests, [{
-        method: 'POST',
-        url: '/protected/data',
-        files: {
-          '10000.clinic-doctor/a.txt': 'a',
-          '10000.clinic-doctor/b.txt': 'b',
-          '10000.clinic-doctor/c.txt': 'c'
-        }
-      }])
+    t.strictDeepEqual(stdout.trim().split('\n'), [
+      `Uploading private data for user test@test.com for ${doctorADirectory} and ${doctorADirectory}.html to ${server.uploadUrl}`,
+      `The data has been uploaded to private area for user test@test.com`,
+      `Thanks for contacting NearForm, we will reply as soon as possible.`
+    ])
 
-      server.close(() => t.end())
-    })
+    t.strictDeepEqual(server.requests, [{
+      method: 'POST',
+      url: '/protected/data',
+      files: {
+        '10000.clinic-doctor/a.txt': 'a',
+        '10000.clinic-doctor/b.txt': 'b',
+        '10000.clinic-doctor/c.txt': 'c'
+      }
+    }])
   })
 })
 
@@ -50,27 +55,29 @@ test('clinic ask 10000.clinic-doctor with default upload url', function (t) {
     '--auth-method', 'simpleSuccess',
     doctorADirectory
   ], function (err, stdout) {
+    t.plan(2)
     // error is expected because the actual server on upload.clinicjs.org will return 404
     t.ok(err)
     t.strictDeepEqual(stdout.trim().split('\n'), [
       `Uploading private data for user test@test.com for ${doctorADirectory} and ${doctorADirectory}.html to https://upload.clinicjs.org`
     ])
-    t.end()
   })
 })
 
 test('clinic ask 10000.clinic-doctor auth failure', function (t) {
-  const server = new FakeUploadServer()
-  server.listen(function () {
-    cli({}, [
-      'clinic', 'ask',
-      '--upload-url', server.uploadUrl,
-      '--auth-method', 'fail',
-      doctorADirectory
-    ], function (err, stdout, stderr) {
-      t.notOk(err)
-      t.ok(stderr.includes('Auth artificially failed'))
-      server.close(() => t.end())
-    })
+  cli({}, [
+    'clinic', 'ask',
+    '--upload-url', server.uploadUrl,
+    '--auth-method', 'fail',
+    doctorADirectory
+  ], function (err, stdout, stderr) {
+    t.plan(2)
+    t.notOk(err)
+    t.ok(stderr.includes('Auth artificially failed'))
   })
+})
+
+test('After all', function (t) {
+  t.plan(0)
+  server.close()
 })
--- test/cli-ask-no-args.test.js
+++ test/cli-ask-no-args.test.js
@@ -5,9 +5,8 @@ const cli = require('./cli.js')
 
 test('clinic ask', function (t) {
   cli({}, ['clinic', 'ask'], function (err, stdout) {
-    console.log('stdout', stdout)
+    t.plan(2)
     t.strictDeepEqual(err, new Error('process exited with exit code 1'))
     t.ok(/Clinic Ask[^\w ]/.test(stdout.split('\n')[1]))
-    t.end()
   })
 })

10190c8f7c2a83ddb06d83b9761126d600a6a1f0
mrister <mihovil.rister@gmail.com>
2018-11-15 17:53:46 +0000

    Improve authenticate

--- test/authenticate.test.js
+++ test/authenticate.test.js
@@ -4,34 +4,86 @@ const http = require('http')
 const test = require('tap').test
 const websocket = require('websocket-stream')
 const proxyquire = require('proxyquire')
-const split2 = require('split2')
-const { ReadableStreamBuffer } = require('stream-buffers')
+let server, cliToken
+let simulateTimeout = false
+let simulateNoToken = false
+
+test('Before all', function (t) {
+  server = http.createServer(() => {})
+
+  websocket.createServer({ server }, conn => {
+    conn.on('data', token => {
+      cliToken = token.toString('utf8')
+      if (simulateTimeout) {
+        conn.write('timeout\n')
+      } else if (simulateNoToken) {
+        conn.write('\n')
+      } else {
+        conn.write('jwtToken\n')
+      }
+      conn.end()
+    })
+  })
+
+  server.listen(0, function () {
+    t.plan(1)
+    t.ok(server)
+  })
+})
 
 test('authenticate', function (t) {
-  let openedUrl, cliToken
+  let openedUrl
   const opnStub = url => {
     openedUrl = url
   }
+
   const authenticate = proxyquire('../lib/authenticate', { 'opn': opnStub }) // mocking the browser opening
 
-  const server = http.createServer(() => {})
-  websocket.createServer({ server }, conn => {
-    conn.pipe(split2())
-      .on('data', token => {
-        cliToken = token
-        const readBuffer = new ReadableStreamBuffer()
-        readBuffer.pipe(conn)
-        readBuffer.put('jwtToken\n')
-        readBuffer.stop()
-      })
-  })
+  return authenticate(`http://127.0.0.1:${server.address().port}`)
+    .then(jwtToken => {
+      t.plan(2)
+      t.strictEqual(openedUrl.split('token=')[1], cliToken)
+      t.strictEqual(jwtToken, 'jwtToken')
+    })
+})
 
-  server.listen(0, () => {
-    authenticate(`http://127.0.0.1:${server.address().port}`)
-      .then(jwtToken => {
-        t.strictEqual(openedUrl.split('token=')[1], cliToken)
-        t.strictEqual(jwtToken, 'jwtToken')
-        server.close(() => t.end())
-      })
-  })
+test('authenticate timeout', function (t) {
+  const opnStub = url => url
+
+  const authenticate = proxyquire('../lib/authenticate', { 'opn': opnStub }) // mocking the browser opening
+
+  simulateTimeout = true
+  return authenticate(`http://127.0.0.1:${server.address().port}`)
+    .then(() => {
+      simulateTimeout = false
+      t.fail('it should reject')
+    })
+    .catch(err => {
+      t.plan(2)
+      t.ok(err)
+      t.ok(err.message.includes('Authentication timed out'))
+      simulateTimeout = false
+    })
+})
+
+test('authenticate no auth token', function (t) {
+  const authenticate = proxyquire('../lib/authenticate', { 'opn': url => url }) // mocking the browser opening
+
+  simulateNoToken = true
+  return authenticate(`http://127.0.0.1:${server.address().port}`)
+    .then(() => {
+      simulateNoToken = false
+      t.fail('it should reject')
+    })
+    .catch(err => {
+      t.plan(2)
+      t.ok(err)
+      t.ok(err.message.includes('Authentication failed. No token obtained'))
+      simulateNoToken = false
+    })
+})
+
+test('After all', function (t) {
+  t.plan(0)
+  server.close()
 })

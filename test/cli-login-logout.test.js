5bab929d7651709fae046a25a2b4b9dd10bba11a
Renée Kooi <renee@kooi.me>
2019-01-09 15:33:08 +0100

    Test tweaks

--- test/cli-login-logout.test.js
+++ test/cli-login-logout.test.js
@@ -6,17 +6,24 @@ const tmp = require('tmp')
 const cli = require('./cli.js')
 const FakeUploadServer = require('./fake-upload-server.js')
 let server
+let server2
 let tempCredentials
 
 // JWT containing test@test.com
 const successfulJwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE1NDIyMjI5MzMsImV4cCI6MTg4OTM3ODEzMywiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoidGVzdCIsImVtYWlsIjoidGVzdEB0ZXN0LmNvbSJ9.FO0v4OM2V23lAXIcv-qcfFo0snOrOmsrY82kmcYlrJI'
+// JWT containing other@email.com
+const successfulJwt2 = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE1NDcwNDQwOTIsImV4cCI6MTg2MjY2MzI5MiwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoiYmx1YiIsImVtYWlsIjoib3RoZXJAZW1haWwuY29tIn0.4OO5_5P3tEf-2Ggq9wmWWFGXM9p1NGmBnWX5bfCLVxE'
 
 test('Before all', function (t) {
+  t.plan(2)
   tempCredentials = tmp.dirSync()
   server = new FakeUploadServer()
   server.listen(function () {
-    t.plan(1)
     t.ok(server)
+  })
+  server2 = new FakeUploadServer()
+  server2.listen(function () {
+    t.ok(server2)
   })
 })
 
@@ -54,7 +61,9 @@ test('clinic user exits with 1 if not authenticated', function (t) {
 })
 
 test('clinic user lists all authed users', function (t) {
-  t.plan(3)
+  t.plan(7)
+
+  let after = 2
 
   cli({
     env: {
@@ -64,8 +73,23 @@ test('clinic user lists all authed users', function (t) {
   }, [
     'clinic', 'login',
     '--upload-url', server.uploadUrl
-  ], function (err) {
+  ], next)
+
+  cli({
+    env: {
+      CLINIC_CREDENTIALS: path.join(tempCredentials.name, '.clinic-user-all'),
+      CLINIC_JWT: successfulJwt2
+    }
+  }, [
+    'clinic', 'login',
+    '--upload-url', server2.uploadUrl
+  ], next)
+
+  function next (err) {
     t.ifError(err)
+    after -= 1
+    if (after > 0) return
+
     cli({
       env: {
         CLINIC_CREDENTIALS: path.join(tempCredentials.name, '.clinic-user-all')
@@ -74,7 +98,42 @@ test('clinic user lists all authed users', function (t) {
       'clinic', 'user'
     ], function (err, stdout) {
       t.ifError(err)
-      t.ok(stdout)
+      const userInfos = stdout.split('\n\n').sort((a, b) => {
+        if (a.includes('test@test')) return -1
+        if (a.includes('other@email')) return 1
+        return 0
+      })
+      t.ok(userInfos[0].includes(server.uploadUrl))
+      t.ok(userInfos[0].includes('Authenticated as test@test.com.'))
+      t.ok(userInfos[1].includes(server2.uploadUrl))
+      t.ok(userInfos[1].includes('Authenticated as other@email.com.'))
+    })
+  }
+})
+
+test('clinic user --upload-url lists single user', function (t) {
+  t.plan(3)
+
+  cli({
+    env: {
+      CLINIC_CREDENTIALS: path.join(tempCredentials.name, '.clinic-user-one'),
+      CLINIC_JWT: successfulJwt
+    }
+  }, [
+    'clinic', 'login',
+    '--upload-url', server.uploadUrl
+  ], function (err) {
+    t.ifError(err)
+
+    cli({
+      env: {
+        CLINIC_CREDENTIALS: path.join(tempCredentials.name, '.clinic-user-one')
+      }
+    }, [
+      'clinic', 'user'
+    ], function (err, stdout) {
+      t.ifError(err)
+      t.ok(stdout.includes('Authenticated as test@test.com.'))
     })
   })
 })
@@ -82,5 +141,5 @@ test('clinic user lists all authed users', function (t) {
 test('After all', function (t) {
   t.plan(0)
   server.close()
-  tempCredentials.removeCallback()
+  server2.close()
 })

974ed78a5c8d96fc54bcaf7af14d639c36e537f8
DylanC <dylan.coakley@nearform.com>
2020-02-11 12:48:55 +0000

    Fix all tests for terminal links (#210)
    * Fixed test for bubbleprof full
    
    * Fix for bubbleprof visualize test
    
    * Fix for doctor full test
    
    * Another fix for bubbleprof visualize
    
    * Fix for doctor visualize test
    
    * Fixes for flame full and doctor visualize tests
    
    * Fix for flame visualize test - all tests passing

--- test/cli-bubbleprof-visualize-only.test.js
+++ test/cli-bubbleprof-visualize-only.test.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const url = require('url')
 const fs = require('fs')
 const path = require('path')
 const test = require('tap').test
@@ -23,7 +24,7 @@ test('clinic bubbleprof --collect-only - no issues', function (t) {
       t.ifError(err)
       t.strictEqual(
         stdout,
-        `Generated HTML file is ${dirpath}.html
+        `Generated HTML file is ${url.pathToFileURL(dirpath)}.html
 You can use this command to upload it:
 clinic upload ${dirpath}
 `)
@@ -67,7 +68,7 @@ test('clinic bubbleprof --visualize-only - with trailing /', function (t) {
       t.ifError(err)
       t.strictEqual(
         stdout,
-        `Generated HTML file is ${dirpath}.html
+        `Generated HTML file is ${url.pathToFileURL(dirpath)}.html
 You can use this command to upload it:
 clinic upload ${dirpath}
 `)

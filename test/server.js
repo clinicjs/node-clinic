43b0e2ed79795f738161a7911216aaa8b6f020f8
James M Snell <jasnell@gmail.com>
2020-11-24 15:56:15 -0800

    Update dependencies, bump to 8.0.0
    Ask/Upload is removed

--- test/server.js
+++ test/server.js
@@ -6,6 +6,8 @@ http.createServer(handler).listen(0)
 
 function handler (req, res) {
   // make this slow
+  /* eslint-disable no-empty */
   for (let i = 0; i < 1000000; i++) {}
+  /* eslint-enable no-empty */
   res.end('ok')
 }

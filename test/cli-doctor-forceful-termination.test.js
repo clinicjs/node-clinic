abdb53b325900ab9b85f16d29e8c072c83701d32
Carlos Espa <carlos.espa@nearform.com>
2023-02-22 20:44:14 +0100

    test: test fixed

--- test/cli-doctor-forceful-termination.test.js
+++ test/cli-doctor-forceful-termination.test.js
@@ -3,20 +3,15 @@
 const path = require('path')
 const test = require('tap').test
 const cli = require('./cli.js')
-const fs = require('fs')
 
 test('clinic doctor throws error when process is forcefully closed before processstat file is generated', { skip: process.version.startsWith('v14') }, function (t) {
   // collect data
   cli({}, [
     'clinic', 'doctor', '--no-open',
     '--', 'node', path.join(__dirname, 'forceful-termination.js')
-  ], function (err, stdout, stderr, tempdir) {
-    console.log(stdout)
-    console.log(stderr)
-    const dirname = stdout.match(/(\.clinic[/\\]\d+.clinic-doctor)/)[1]
+  ], function (err, stdout, stderr) {
     t.ok(err)
     t.ok(stderr.includes('Process forcefully closed before processstat file generation'))
-    t.ok(fs.existsSync(path.resolve(tempdir, dirname + '-processstat')))
     t.end()
   })
 })
